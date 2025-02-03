625e10953e06d63c0d70f2ba43eba7caea75e35a
Baister <gustavobaister@gmail.com>
2025-02-03 10:48:54 -0300

    Opcoes de menu

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -13,10 +13,13 @@ namespace Cronometro
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundos");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundos => 10s = 10 segundos.");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto.");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower(); // Não é feito um parse com isso, até porque ele é string
+            Console.WriteLine(data);
         }
 
         static void Start(int time)
